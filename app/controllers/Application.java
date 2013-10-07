10cd5b17ddeb795ef82fc148b93457121a23379b
Eli <laue09@gmail.com>
2013-10-06 19:44:41 -1000

    updating

---
+++ app/controllers/Application.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.Index;
+import views.html.Page1;
+import views.html.Skip;
+import views.html.Megan;
+import views.html.Kyuss;
+
+/**
+ * Implements the controllers for this application.
+ */
+public class Application extends Controller {
+
+  /**
+   * Returns the home page. 
+   * @return The resulting home page. 
+   */
+  public static Result index() {
+    return ok(Index.render("Welcome to the home page."));
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result page1() {
+    return ok(Page1.render("Welcome to Page1."));
+    
+  }
+  
+  public static Result Skip() {
+    return ok(Skip.render(""));
+    
+  }
+  
+  public static Result Megan() {
+    return ok(Megan.render(""));
+    
+  }
+  
+  public static Result Kyuss() {
+    return ok(Kyuss.render(""));
+    
+  }
+}
